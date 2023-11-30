65107a370cdb71c1d0ce540f588ae7b53433aa06
Lunge12 <thedestroyer9155555@gmail.com>
2023-11-29 20:16:58 -0600

    added login page

---
+++ src/Components/Login.js
@@ -0,0 +1,24 @@
+// Login.js
+import React from 'react';
+import '../Styles/Login.css';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
