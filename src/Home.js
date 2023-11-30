636bf4edc9f0933b6a58926d4d88d83566f66c8c
lawrnfy <yonishassan23@gmail.com>
2023-11-30 16:43:46 -0600

    Home page changed

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,57 @@
-// Home.js
-import React from 'react';
+// home.js
 
-function Home() {
-  return <div>Home Page</div>;
-}
+import React, { useState } from 'react';
+import './Home.css'; // Import the CSS file for styling
+
+const Home = () => {
+  const [schedule, setSchedule] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setSchedule([...schedule, { id: Date.now(), task }]);
+      setTask('');
+    }
+  };
+
+  const removeTask = (taskId) => {
+    const updatedSchedule = schedule.filter((item) => item.id !== taskId);
+    setSchedule(updatedSchedule);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Schedule Planner</h1>
+      </header>
+      <section className="content">
+        <div className="schedule-container">
+          <h2>Your Schedule</h2>
+          <ul>
+            {schedule.map((item) => (
+              <li key={item.id}>
+                {item.task}
+                <button onClick={() => removeTask(item.id)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="add-task-container">
+          <h2>Add Task</h2>
+          <input
+            type="text"
+            placeholder="Enter task..."
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+      </section>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Schedule Planner</p>
+      </footer>
+    </div>
+  );
+};
 
 export default Home;
