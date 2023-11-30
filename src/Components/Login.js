9c5472edb2d477a84df1d58cdf61a63348bb8d64
YarnGrowler <yarngrowler@gmail.com>
2023-11-29 20:36:21 -0600

    Better Login Page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,17 +5,25 @@ import '../Styles/Login.css';
 function Login() {
   return (
     <div className="login-container">
-      <div className="login-form">
-        <h2>Login</h2>
-        <form>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+      <div className="login-card">
+        <div className="left-section">
+          <h1>Welcome Back!</h1>
+          <p>Enter your credentials to access your account.</p>
+        </div>
+        <div className="right-section">
+          <div className="login-form">
+            <h2>Login</h2>
+            <form>
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" name="username" placeholder="Enter your username" required />
 
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" placeholder="Enter your password" required />
 
-          <button type="submit">Login</button>
-        </form>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
