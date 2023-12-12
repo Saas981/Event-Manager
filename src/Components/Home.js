a854af02b82b7307292f51389349b56d4a6ceca1
YarnGrowler <yarngrowler@example.com>
2023-12-12 13:16:07 -0600

    Hide show navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,103 +1,67 @@
 import React from 'react';
-import {
-  Container,
-  Typography,
-  Grid,
-  Card,
-  CardContent,
-  Button,
-  Tooltip,
-  Box,
-  IconButton,
-  Avatar,
-  Skeleton,
-} from '@mui/material';
-import '@fontsource/poppins'; // Import Poppins font
-import EventIcon from '@mui/icons-material/Event'; // Import Material-UI Event icon
-import AlarmOnIcon from '@mui/icons-material/AlarmOn'; // Import Material-UI AlarmOn icon
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Import Material-UI AccountCircle icon
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'; // Import Material-UI KeyboardArrowRight icon
+import { Container, Typography, Button, Grid, Skeleton, Paper } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Home = () => {
   return (
-    <Container className="home-container" maxWidth="lg">
-      <Box textAlign="center" my={4}>
-        <Typography variant="h2" component="div" gutterBottom style={{ fontFamily: 'Poppins, sans-serif' }}>
-          Welcome to Your Event Manager
+    <Container className="home-container" maxWidth="md" style={{ textAlign: 'center', }}>
+      <Paper elevation={1} style={{ padding: '30px', marginBottom: '30px' }}>
+        <Typography variant="h3" component="div" gutterBottom style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 'bold', color: '#000' }}>
+          Elevate Your Events with Ease
         </Typography>
-        <Typography variant="h5" color="textSecondary" component="div" style={{ fontFamily: 'Poppins, sans-serif' }}>
-          Simplify, Organize, and Manage Your Events with Ease
-        </Typography>
-      </Box>
 
-      {/* Features Section */}
+        <Typography variant="h6" color="textSecondary" style={{ marginTop: '20px', color: '#000' }}>
+          Simplify, Organize, and Manage Your Events Like Never Before
+        </Typography>
+      </Paper>
+
       <Grid container spacing={3} justifyContent="center">
-        <Grid item xs={12} sm={6} md={4}>
-          <Card elevation={3} className="feature-card">
-            <CardContent>
-              <Avatar className="feature-icon">
-                <EventIcon />
-              </Avatar>
-              <Typography variant="h6" className="feature-title">Easy Event Creation</Typography>
-              <Typography variant="body2" color="textSecondary">
-                Create events effortlessly with our user-friendly interface.
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} style={{ padding: '30px', marginBottom: '20px', minHeight: '300px' }}>
+            <Skeleton variant="rectangular" width="100%" height={150} animation="wave" style={{ borderRadius: '8px' }} />
+            <Typography variant="h6" style={{ marginTop: '20px', color: '#000' }}>
+              Effortless Event Creation
+            </Typography>
+            <Typography variant="body2" color="textSecondary" style={{ color: '#000' }}>
+              Create events effortlessly with our user-friendly interface.
+            </Typography>
+          </Paper>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4}>
-          <Card elevation={3} className="feature-card">
-            <CardContent>
-              <Avatar className="feature-icon">
-                <AlarmOnIcon />
-              </Avatar>
-              <Typography variant="h6" className="feature-title">Efficient Invitations</Typography>
-              <Typography variant="body2" color="textSecondary">
-                Invite participants using email addresses or usernames.
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} style={{ padding: '30px', marginBottom: '20px', minHeight: '300px' }}>
+            <Skeleton variant="rectangular" width="100%" height={150} animation="wave" style={{ borderRadius: '8px' }} />
+            <Typography variant="h6" style={{ marginTop: '20px', color: '#000' }}>
+              Efficient Invitations
+            </Typography>
+            <Typography variant="body2" color="textSecondary" style={{ color: '#000' }}>
+              Invite participants using email addresses or usernames.
+            </Typography>
+          </Paper>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4}>
-          <Card elevation={3} className="feature-card">
-            <CardContent>
-              <Avatar className="feature-icon">
-                <AccountCircleIcon />
-              </Avatar>
-              <Typography variant="h6" className="feature-title">Streamlined RSVPs</Typography>
-              <Typography variant="body2" color="textSecondary">
-                Easily manage and track RSVPs for your events.
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} style={{ padding: '30px', marginBottom: '20px', minHeight: '300px' }}>
+            <Skeleton variant="rectangular" width="100%" height={150} animation="wave" style={{ borderRadius: '8px' }} />
+            <Typography variant="h6" style={{ marginTop: '20px', color: '#000' }}>
+              Streamlined RSVPs
+            </Typography>
+            <Typography variant="body2" color="textSecondary" style={{ color: '#000' }}>
+              Easily manage and track RSVPs for your events.
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
 
-      {/* CTA Section */}
-      <Box textAlign="center" my={4}>
-        <Typography variant="h4" gutterBottom style={{ fontFamily: 'Poppins, sans-serif' }}>
-          Ready to Get Started?
-        </Typography>
-        <Button variant="contained" color="primary" endIcon={<KeyboardArrowRightIcon />}>
-          Sign Up Now
-        </Button>
-      </Box>
-
-      {/* Image Section with Skeleton */}
-      <Box textAlign="center" my={4}>
-        <Skeleton variant="rectangular" width="100%" height={400} animation="wave" />
-      </Box>
-
-      {/* Tooltip Section */}
-      <Box textAlign="center" my={4}>
-        <Tooltip title="Explore Features" arrow>
-          <IconButton color="primary" size="large">
-            <i className="fas fa-info-circle"></i>
-          </IconButton>
-        </Tooltip>
-      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        endIcon={<ArrowForwardIcon />}
+        style={{ borderRadius: '12px', marginTop: '30px', backgroundColor: '#FF6B6B', color: '#fff' }}
+      >
+        Get Started Now
+      </Button>
     </Container>
   );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,3 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
@@ -13,14 +12,22 @@ import {
   useTheme,
   Menu,
   MenuItem,
+  Slide,
 } from '@mui/material';
-import { Home, Info, ContactMail,Login, Person, PersonAdd, Menu as MenuIcon } from '@mui/icons-material';
+import { Home, Info, ContactMail, Login, Person, PersonAdd, Menu as MenuIcon } from '@mui/icons-material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import '../Styles/Navbar.css';
 
 const Navbar = ({ user, signOut }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    target: window,
+    threshold: 50,
+  });
 
   const handleOpenMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -29,55 +36,55 @@ const Navbar = ({ user, signOut }) => {
   const handleCloseMenu = () => {
     setAnchorEl(null);
   };
-  const pages = ['About', 'Contact', "Login","Signup"];
+
+  const pages = ['About', 'Contact', 'Login', 'Signup'];
+
   return (
-    <AppBar className="navbar" position="static" style={{ opacity:0.6,background: 'linear-gradient(to right, #503F9F, #FF51B5)', borderRadius: 0 }}>
-      <Container >
-        <Toolbar >
-          <IconButton  className="nav-button" edge="start" color="inherit" component={Link} to="/">
-            <Home />
-          </IconButton>
-          <Typography variant="h6" style={{ flexGrow: 1, marginLeft: '10px', fontFamily: 'Poppins, sans-serif' }}>
-           Event Manager
-          </Typography>
-          {isMobile ? (
-            // Hamburger menu for smaller screens
-            <>
-              <IconButton className="nav-button"  edge="end" color="inherit" onClick={handleOpenMenu}>
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleCloseMenu}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-              >
-                {pages.map((page) => (
-                 <MenuItem key={page} onClick={handleCloseMenu}>
-                 <Typography textAlign="center" style={{ fontFamily: 'Poppins, sans-serif' }}>{page}</Typography>
-               </MenuItem>
-                ))}
-              </Menu>
-            </>
-          ) : 
-            (
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar className="navbar" position="fixed" style={{ opacity: 0.9, background: 'linear-gradient(to right, #ae9dfa, #f7abd7)', borderRadius: 0 }}>
+        <Container>
+          <Toolbar >
+            <IconButton className="nav-button" edge="start" color="inherit" component={Link} to="/">
+              <Home />
+            </IconButton>
+            <Typography variant="h6" style={{ flexGrow: 1, marginLeft: '10px', fontFamily: 'Poppins, sans-serif' }}>
+              Event Manager
+            </Typography>
+            {isMobile ? (
+              // Hamburger menu for smaller screens
               <>
-                <Button color="inherit" className="nav-button"  component={Link} to="/about" style={{ marginTop:"4px",marginRight: '30px',fontFamily: 'Poppins, sans-serif'  }}>
-                About 
-                  <Info style={{marginLeft:'5px'}}/>
-                 
+                <IconButton className="nav-button" edge="end" color="inherit" onClick={handleOpenMenu}>
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleCloseMenu}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseMenu}>
+                      <Typography textAlign="center" style={{ fontFamily: 'Poppins, sans-serif' }}>{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <>
+                <Button color="inherit" className="nav-button" component={Link} to="/about" style={{ marginTop: "4px", marginRight: '30px', fontFamily: 'Poppins, sans-serif' }}>
+                  About
+                  <Info style={{ marginLeft: '5px' }} />
                 </Button>
-                <Button color="inherit" className="nav-button"  component={Link} to="/contact" style={{ marginTop:"4px",marginRight: '480px',fontFamily: 'Poppins, sans-serif'  }}>
-                Contact
-                <ContactMail style={{marginLeft:'5px'}}/>
-                
+                <Button color="inherit" className="nav-button" component={Link} to="/contact" style={{ marginTop: "4px", marginRight: '480px', fontFamily: 'Poppins, sans-serif' }}>
+                  Contact
+                  <ContactMail style={{ marginLeft: '5px' }} />
                 </Button>
                 {user ? (
                   // Show profile icon when signed in
@@ -87,19 +94,18 @@ const Navbar = ({ user, signOut }) => {
                   <>
                     <Button color="inherit" className="nav-button" component={Link} to="/login" style={{ marginRight: '1px' }}>
                       <Login />
-                   
                     </Button>
                     <Button color="inherit" className="nav-button" component={Link} to="/signup" style={{ marginRight: '10px' }}>
                       <PersonAdd />
-                    
                     </Button>
                   </>
                 )}
               </>
             )}
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Slide>
   );
 };
 
