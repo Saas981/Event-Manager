5d38d9e41ce6e5195c36fe80ddd080cfb93534ae
YarnGrowler <yarngrowler@example.com>
2023-12-14 12:52:45 -0600

    Login and Nabar Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
@@ -6,10 +6,11 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Login from './Components/Login';
 import SignUp from './Components/Signup';
+import Dashboard from './Components/Dashboard'
 import { Container, Grid } from '@mui/material';
 import './App.css'; 
 
-
+import { getCurrentUser } from 'aws-amplify/auth';
 import config from './aws-exports';
 import { Amplify } from 'aws-amplify';
 Amplify.configure(config);
@@ -17,10 +18,30 @@ Amplify.configure(config);
 // App.js
 
 
-function App({ signOut, user }) {
+
+
+function App({ }) {
+  const [user, setUser] = React.useState(null);
+ 
+  useEffect(() => {
+    const currentAuthenticatedUser = async () => {
+      try {
+        const { username, userId, signInDetails } = await getCurrentUser();
+        console.log(`The username: ${username}`);
+        console.log(`The userId: ${userId}`);
+        console.log(`The signInDetails: ${JSON.stringify(signInDetails)}`);
+        setUser(signInDetails.loginId)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    // Call the function when the component mounts
+    currentAuthenticatedUser();
+  }, [])
   return (
     <Router >
-           <Navbar style={{zIndex:100}}/>
+           <Navbar user={user} style={{zIndex:100}}/>
       <div style={{minHeight: '90vh',height:"auto",background: 'linear-gradient(to right, rgba(80, 63, 159,0.18), rgba(255, 81, 181,0.18))',paddingBottom:"10%",paddingTop:"6%"}} >
    
         <br></br>
@@ -38,6 +59,7 @@ function App({ signOut, user }) {
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<SignUp />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </Grid>
       
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,45 @@
-// Login.js
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Alert, CircularProgress } from '@mui/material';
 import '../Styles/Login.css';
-import { TextField, Button, Alert } from '@mui/material';
-
+import {signIn } from 'aws-amplify/auth'; // Import Auth from aws-amplify
 
 function Login() {
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    username: '', // Change email to username
+    password: '',
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { username, password } = formData; // Change email to username
+
+    try {
+      await signIn({username, password}); // Use Auth.signIn with username
+      setError(null); // Reset error state on successful login
+
+      // Redirect to the dashboard on successful login
+      setLoading(true)
+      await new Promise(resolve => setTimeout(resolve, 1500));
+      setLoading(false)
+      window.location.href = '/dashboard';
+    } catch (error) {
+      console.error('Error signing in:', error);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-card">
@@ -15,26 +50,34 @@ function Login() {
         <div className="right-section">
           <div className="login-form">
             <h2>Login</h2>
-            <form>
-            <TextField
-                    type="email"
-                    id="email"
-                    name="email"
-                    label="Email"
-                    placeholder="Enter your email"
-                    required
-                  />
-                  <TextField
-                    type="password"
-                    id="password"
-                    name="password"
-                    label="Password"
-                    placeholder="Choose a password"
-                    required
-                  />
-                  <Button variant="contained" type="submit">
-                    Sign Up
-                  </Button>
+            <form onSubmit={handleLogin}>
+              <TextField
+                type="text"
+                id="username" // Change email to username
+                name="username"
+                label="Username" // Change email to username
+                placeholder="Enter your username" // Change email to username
+                required
+                onChange={handleChange}
+              />
+              <TextField
+                type="password"
+                id="password"
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                required
+                onChange={handleChange}
+              />
+              <Button variant="contained" type="submit">
+              {loading ? <CircularProgress size={24} color="inherit" /> :"Sign In" }
+
+              </Button>
+              {error && (
+                <div className="alert">
+                  <p>{error}</p>
+                </div>
+              )}
             </form>
           </div>
         </div>
