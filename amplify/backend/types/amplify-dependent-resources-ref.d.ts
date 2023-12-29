6cd52bf66fab02ffcd85a8b69fc489439e11c402
YarnGrowler <yarngrowler@gmail.com>
2023-12-29 09:01:42 -0600

    Amplify Updates

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "EventManager": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "EventManager": {
       "AppClientID": "string",
