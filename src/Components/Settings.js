604de2c3ac9215843928635f63a66cf101e671cf
yusuf7264 <yusufha.2004@gmail.com>
2024-01-12 17:00:30 -0600

    you can change the values of the settings page

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -106,10 +106,10 @@ const Settings = ({ themeType,setTheme,userData,setUserData,theme }) => {
                 <SettingsHeader title="Account Settings" />
                 {/* Sample settings under the "Account Settings" tab */}
                 <SettingItem theme={theme }title="Name" value={`${userData?.name}`} onEdit={handleEdit("name")} />
-                <SettingItem theme={theme }title="Username" value="johndoe123" onEdit={handleEdit("username")} />
-                <SettingItem theme={theme }title="Phone Number" value="123-456-7890" onEdit={handleEdit("phone")} />
+                <SettingItem theme={theme }title="Username" value={`${userData?.username}`} onEdit={handleEdit("username")} />
+                <SettingItem theme={theme }title="Phone Number" value={`${userData?.phone}`} onEdit={handleEdit("phone")} />
                 <SettingItem theme={theme }title="Email Address" value={`${userData?.email}`} onEdit={handleEdit("email")} />
-                <SettingItem theme={theme }title="Password" value="**********"  onEdit={handleEdit("password")} />
+                <SettingItem theme={theme }title="Password" value={`${userData?.password}`}  onEdit={handleEdit("password")} />
                 
                  <Typography variant="h5" mb={3} sx={{  fontFamily: 'Poppins', fontWeight: '600', marginTop: "30px",color:"#292929", textAlign: 'left'}}>
                 Account Deletion
