32149fc68984a65718c4de5ab1faf4acc5d320ad
lawrnfy <yonishassan23@gmail.com>
2024-01-15 14:27:22 -0600

    worked on error page

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Typography, Box, Button } from '@mui/material';
+import GppBadTwoToneIcon from '@mui/icons-material/GppBadTwoTone';
 import { styled } from '@mui/system';
 import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
     <ErrorContainer>
-      <Typography variant="h1" sx={{ textAlign: 'center', fontSize: '4rem', color: '#FF1744', marginBottom: '20px' }}>
+      <GppBadTwoToneIcon sx={{ fontSize: "170px", color: 'red' }} ></GppBadTwoToneIcon>
+      <Typography variant="h1" sx={{ textAlign: 'center', fontSize: '50px', color: '#131717', marginBottom: '20px', fontFamily: "Poppins", fontWeight: "600" }}>
         Oops! Page Not Found
       </Typography>
-      <Typography variant="body1" sx={{ textAlign: 'center', fontSize: '1.5rem', marginBottom: '30px' }}>
-        The requested page could not be found. Please check the URL and try again.
+      <Typography variant="body1" sx={{ textAlign: 'center', fontSize: '1.5rem', marginBottom: '30px', color: '#8e9191', fontWeight: '500' }}>
+        The requested page could not be found.<br></br> Please check the URL and try again.
       </Typography>
       <Link to="/" style={{ textDecoration: 'none' }}>
-        <Button variant="contained" color="primary">
-          Go to Home
-        </Button>
+        <StyledButton variant="contained" color="primary" className="bounce">
+          Go back to the Homepage
+        </StyledButton>
       </Link>
     </ErrorContainer>
   );
@@ -26,8 +28,17 @@ const ErrorContainer = styled(Box)({
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  height: '100vh',
-  padding: '20px',
+  height: '500px',
+  padding: '10px',
+  backgroundColor: 'whitesmoke',
+  borderRadius: '20px', // Adjust the border radius as needed
+  border: '7px solid lightgrey', // Add a black border
+});
+
+const StyledButton = styled(Button)({
+  borderRadius: '20px', // Adjust the border radius as needed
+  background: 'linear-gradient(45deg, #2196F3 0%, #21CBF3 100%)', // Add a light blue gradient
+  color: 'purple', // Text color
 });
 
 export default ErrorPage;
