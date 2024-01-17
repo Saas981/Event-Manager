91db1f9c08af34d35e968d848b7f95c2f52adc61
YarnGrowler <yarngrowler@gmail.com>
2024-01-16 18:46:59 -0600

    Password Reset Flow Added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { TextField, Button, Alert, CircularProgress } from '@mui/material';
 import '../Styles/Login.css';
-import {Auth } from 'aws-amplify'; // Import Auth from aws-amplify
+import { Auth } from 'aws-amplify'; // Import Auth from aws-amplify
 
-function Login({theme}) {
+function Login({ theme }) {
   const [loading, setLoading] = useState(false);
 
   const [formData, setFormData] = useState({
@@ -11,50 +11,49 @@ function Login({theme}) {
     password: '',
   });
 
+  const[screen,setScreen]= useState(1)
+
+  const [resetPasswordData, setResetPasswordData] = useState({
+    resetUsername: '',
+    resetCode: '',
+    newPassword: '',
+  });
+
   const [error, setError] = useState(null);
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+
 
   async function handleSignOut() {
     try {
-      setLoading(true); // Set loading to true when starting sign-out
+      setLoading(true);
       await Auth.signOut();
-      await new Promise(resolve => setTimeout(resolve, 1500));
-      window.location.reload(); // Reload the page after successful sign-out
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      window.location.reload();
       window.location.href = '/login';
-
     } catch (error) {
       console.log('error signing out: ', error);
     } finally {
-      setLoading(false); // Set loading to false whether sign-out succeeds or fails
+      setLoading(false);
     }
   }
 
-
   const handleLogin = async (e) => {
     e.preventDefault();
-  
+
     const { username, password } = formData;
-  
+
     try {
-      await Auth.signIn({username, password});
+      await Auth.signIn({ username, password });
       setError(null);
       setLoading(true);
-      await new Promise(resolve => setTimeout(resolve, 1500));
+      await new Promise((resolve) => setTimeout(resolve, 1500));
       setLoading(false);
       window.location.href = '/dashboard';
     } catch (error) {
       console.error('Error signing in:', error);
-      if (error.message === "There is already a signed in user.") {
-        // If there is already a signed-in user, sign them out first and then attempt login again
+      if (error.message === 'There is already a signed in user.') {
         try {
-          await Auth.signOut(); // Sign out the current user
-          // Now attempt login again
+          await Auth.signOut();
           await handleLogin(e);
         } catch (signOutError) {
           console.error('Error signing out:', signOutError);
@@ -65,25 +64,70 @@ function Login({theme}) {
       }
     }
   };
-  
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <div className="left-section">
-          <h1 className='left-section-text authTitle'>Welcome Back!</h1>
-          <p className='left-section-text'>Enter your credentials to access your account.</p>
-        </div>
-        <div className="right-section">
-          <div className="login-form">
-            <h2>Login</h2>
+  const handleResetPasswordChange = (e) => {
+    console.log("RESET DATA ",e)
+    setResetPasswordData({
+      ...resetPasswordData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleForgotPassword = async (e) => {
+    e.preventDefault();
+    let username =resetPasswordData.resetUsername
+    if(!username){
+      username = formData.username
+    }
+    console.log("USRNEANME ",username)
+    try {
+      await Auth.forgotPassword(username);
+      console.log('Password reset code sent successfully!');
+      setScreen(3); // Move to the next screen
+    } catch (error) {
+      console.error('Error sending password reset code:', error);
+      setError(error.message);
+    }
+  };
+
+  const handleForgotPasswordSubmit = async (e) => {
+    e.preventDefault();
+      let username =resetPasswordData.resetUsername
+    if(!username){
+      username = formData.username
+    }
+    const { resetUsername, resetCode, newPassword } = resetPasswordData;
+
+    try {
+      await Auth.forgotPasswordSubmit(username, resetCode, newPassword);
+      console.log('Password reset successful!');
+      setScreen(1); // Move to the final screen
+    } catch (error) {
+      console.error('Error submitting password reset:', error);
+      setError(error.message);
+    }
+  };
+
+
+  const renderForm = () => {
+    switch (screen) {
+      case 1:
+        return (
+          <>
+   <h2>Login</h2>
             <form onSubmit={handleLogin}>
               <TextField
                 type="text"
-                id="username" // Change email to username
+                id="username"
                 name="username"
-                label="Username" // Change email to username
-                placeholder="Enter your username" // Change email to username
+                label="Username"
+                placeholder="Enter your username"
                 required
                 onChange={handleChange}
               />
@@ -97,8 +141,7 @@ function Login({theme}) {
                 onChange={handleChange}
               />
               <Button variant="contained" type="submit">
-              {loading ? <CircularProgress size={24} color="inherit" /> :"Sign In" }
-
+                {loading ? <CircularProgress size={24} color="inherit" /> : 'Sign In'}
               </Button>
               {error && (
                 <div className="alert">
@@ -106,6 +149,96 @@ function Login({theme}) {
                 </div>
               )}
             </form>
+              <Button onClick={()=>{setScreen(2)}}>
+                  Forgot Password
+                </Button>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <h2>Forgot Password</h2>
+            <form onSubmit={handleForgotPassword}>
+              <TextField
+                type="text"
+                id="resetUsername"
+                name="resetUsername"
+                label="Username"
+                placeholder="Enter your username"
+                required
+                onChange={handleResetPasswordChange}
+              />
+              <Button variant="contained" type="submit">
+                Send Reset Code
+              </Button>
+               {error && (
+                <div className="alert">
+                  <p>{error}</p>
+                </div>
+              )}
+            </form>
+          </>
+        );
+      case 3:
+        return (
+          <>
+               <h2>Reset Password</h2>
+            <form onSubmit={handleForgotPasswordSubmit}>
+              <TextField
+                type="text"
+                id="resetCode"
+                name="resetCode"
+                label="Reset Code"
+                placeholder="Enter the code sent to your email"
+                required
+                onChange={handleResetPasswordChange}
+              />
+              <TextField
+                type="password"
+                id="newPassword"
+                name="newPassword"
+                label="New Password"
+                placeholder="Enter your new password"
+                required
+                onChange={handleResetPasswordChange}
+              />
+              <Button variant="contained" type="submit">
+                Reset Password
+              </Button>
+               {error && (
+                <div className="alert">
+                  <p>{error}</p>
+                </div>
+              )}
+            </form>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+
+
+
+
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <div className="left-section">
+          <h1 className='left-section-text authTitle'>Welcome Back!</h1>
+          <p className='left-section-text'>Enter your credentials to access your account.</p>
+        </div>
+        <div className="right-section">
+          <div className="login-form">
+         
+{renderForm()}
+            {/* Forgot Password Form */}
+           
+
+            {/* Forgot Password Submit Form */}
+        
           </div>
         </div>
       </div>
